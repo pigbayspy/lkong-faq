4dd8795a24eae464258fbeca6465f9adba375b30
pigbayspy <pigbayspy@gmail.com>
2021-09-27 22:37:05 +0800

    feat:使用 antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import './App.css';
+import { ConfigProvider, Layout, Typography } from 'antd';
+import zhCN from 'antd/lib/locale/zh_CN';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+import "antd/dist/antd.css";
+
+moment.locale("zh-cn");
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <ConfigProvider locale={zhCN}>
+      <Layout style={{ margin: "0 auto", maxWidth: "480px" }}>
+        <Layout.Content style={{ padding: '10px 50px' }}>
+          <Typography>
+            <Typography.Title>龙空 APP 帮助</Typography.Title>
+            <Typography.Text>帮助内容</Typography.Text>
+          </Typography>
+        </Layout.Content>
+      </Layout>
+    </ConfigProvider>
   );
 }
 
