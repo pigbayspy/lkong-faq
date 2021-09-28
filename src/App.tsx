6ee98d63081c4e14f9c9e3ed3ff4239a2840f173
Yin Hang <pigbayspy@gmail.com>
2021-09-28 15:08:38 +0800

    feat:修改样式

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,22 @@ moment.locale("zh-cn");
 
 function App() {
   return (
-    <ConfigProvider locale={zhCN}>
-      <Layout style={{ margin: "0 auto", maxWidth: "480px" }}>
-        <Layout.Content style={{ padding: '10px 50px' }}>
-          <Typography>
-            <Typography.Title>龙空 APP 帮助</Typography.Title>
-            <Typography.Text>帮助内容</Typography.Text>
-          </Typography>
-        </Layout.Content>
-      </Layout>
-    </ConfigProvider>
+      <ConfigProvider locale={zhCN}>
+          <Layout style={{margin: "0 auto", maxWidth: "480px", minHeight: "960px"}}>
+              <Layout.Content style={{padding: '0 24px', marginTop: "36px"}}>
+                  <Typography>
+                      <Typography.Title>龙空 APP 帮助</Typography.Title>
+                      <Typography.Text>帮助内容</Typography.Text>
+                  </Typography>
+              </Layout.Content>
+              <Layout.Footer
+                  style={{textAlign: 'center', background: "#f0f2f5", paddingLeft: '24px', paddingRight: "24px"}}>
+                  <Typography>
+                      <Typography.Text>感谢支持</Typography.Text>
+                  </Typography>
+              </Layout.Footer>
+          </Layout>
+      </ConfigProvider>
   );
 }
 
